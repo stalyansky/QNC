e753015e9b0335b39eac3650f808bb60c4f1024d
stalyansky <33562509+stalyansky@users.noreply.github.com>
2025-09-02 17:45:32 -0400

    Update frequentist_vs_bayesian.py

--- frequentist_vs_bayesian.py
+++ frequentist_vs_bayesian.py
@@ -1,6 +1,6 @@
 #Written by Seth Talyansky on September 2, 2025
 ##Exercise 1
-#A positive test result is not significant at the P < 0.05 level because P = 0.05 (5% of the time the test yields a positive result in an individual without HIV, i.e., given the null hypothesis that HIV is absent)
+#A positive test result is not significant at the P < 0.05 level because for this test P = 0.05 (5% of the time the test yields a positive result in an individual without HIV, i.e., given the null hypothesis that HIV is absent)
 ##Exercise 2
 #Bayes' theorem: P(HIV|+) = P(+|HIV) * P(HIV)/P(+), where P(+|HIV) is the true positive rate, P(HIV) is the prevalence of HIV, and P(+) is the true positive rate (1 minus the false negative rate) plus the false positive rate
 import numpy as np
