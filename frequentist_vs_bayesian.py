317b21c653a1bd50b81b0fa99dab4c973b8effce
stalyansky <33562509+stalyansky@users.noreply.github.com>
2025-09-02 17:44:45 -0400

    Update frequentist_vs_bayesian.py

--- frequentist_vs_bayesian.py
+++ frequentist_vs_bayesian.py
@@ -9,3 +9,17 @@ false_positive = 0.05
 for prevalence in np.arange(0, 1.1, 0.1):
     p_hiv = true_positive * prevalence/(true_positive+false_positive)
     print('When the prevalence is %d%%, the probability of infection given a positive test result is %d%%.' %(100*prevalence, 100*p_hiv))
+#Output:
+'''
+When the prevalence is 0%, the probability of infection given a positive test result is 0%.
+When the prevalence is 10%, the probability of infection given a positive test result is 9%.
+When the prevalence is 20%, the probability of infection given a positive test result is 19%.
+When the prevalence is 30%, the probability of infection given a positive test result is 28%.
+When the prevalence is 40%, the probability of infection given a positive test result is 38%.
+When the prevalence is 50%, the probability of infection given a positive test result is 47%.
+When the prevalence is 60%, the probability of infection given a positive test result is 57%.
+When the prevalence is 70%, the probability of infection given a positive test result is 66%.
+When the prevalence is 80%, the probability of infection given a positive test result is 76%.
+When the prevalence is 90%, the probability of infection given a positive test result is 85%.
+When the prevalence is 100%, the probability of infection given a positive test result is 95%.
+'''
